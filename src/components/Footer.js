9be901a6fbaa43f661ae79b6ed0ac6e2629c21f2
Sam13 <richou.samuel13@gmail.com>
2025-03-28 00:27:32 +0100

    hhyu

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,17 +1,19 @@
 import React from 'react';
+import { Link, useLocation } from 'react-router-dom';
 import { useTranslation } from "react-i18next";
 
 const Footer = () => {
     const { t } = useTranslation();
+    const location = useLocation();
     return (
         <div className="footer">
             <div className="footer-container">
                 <nav>
                     <ul>
-                        <li><a href="/">{t("nav1")}</a></li>
-                        <li><a href="projects">{t("nav2")}</a></li>
-                        <li><a href="/Create-space">{t("nav3")}</a></li>
-                        <li><a href="/contact">{t("nav4")}</a></li>
+                        <Link to="/" className={location.pathname === "/" ? "active" : ""}>{t("nav1")}</Link>
+                        <Link to="/projects" className={location.pathname === "/projects" ? "active" : ""}>{t("nav2")}</Link>
+                        <Link to="/create-space" className={location.pathname === "/create-space" ? "active" : ""}>{t("nav3")}</Link>
+                        <Link to="/contact" className={location.pathname === "/contact" ? "active" : ""}>{t("nav4")}</Link>
                     </ul>
                 </nav>
             </div>
